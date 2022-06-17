2f8675af088de538e5ad6a8b6e8d55457527fbd8
Dennis Oelkers <dennis@graylog.com>
2022-06-17 15:12:16 +0200

    Improving test.

--- graylog2-web-interface/src/components/common/LinkContainer.test.tsx
+++ graylog2-web-interface/src/components/common/LinkContainer.test.tsx
@@ -15,15 +15,13 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 import * as React from 'react';
-import { render, waitFor, screen } from 'wrappedTestingLibrary';
+import { render, screen, waitFor, screen } from 'wrappedTestingLibrary';
 import { fireEvent } from '@testing-library/react';
+import { Route, Routes } from 'react-router-dom';
 
-import history from 'util/History';
 import { Button } from 'components/bootstrap';
 
 import { LinkContainer } from './router';
-
-jest.mock('util/History');
 
 describe('LinkContainer', () => {
   const hasHref = (element: HTMLElement | HTMLAnchorElement): element is HTMLAnchorElement => {
@@ -35,15 +33,25 @@ describe('LinkContainer', () => {
   });
 
   it('should use component passed in children', async () => {
-    render(
-      <LinkContainer to="/">
-        <Button bsStyle="info">All Alerts</Button>
-      </LinkContainer>,
-    );
+    render((
+      <Routes>
+        <Route path="/"
+               element={(
+                 <LinkContainer to="/alerts">
+                   <Button bsStyle="info">All Alerts</Button>
+                 </LinkContainer>
+                 )} />
+        <Route path="/alerts" element={<span>Hello world!</span>} />
+      </Routes>
+    ));
 
-    fireEvent.click(await screen.findByText('All Alerts'));
+    const button = await screen.findByText('All Alerts');
 
-    expect(history.push).toHaveBeenCalledWith('/');
+    expect(screen.queryByText('Hello world!')).not.toBeInTheDocument();
+
+    fireEvent.click(button);
+
+    await screen.findByText('Hello world!');
   });
 
   it('should call onClick', async () => {
