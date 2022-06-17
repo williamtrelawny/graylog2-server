60bb82023aed585f021ab125cd04f2d069e89e42
Dennis Oelkers <dennis@graylog.com>
2022-06-17 16:01:45 +0200

    Updating test.

--- graylog2-web-interface/src/views/components/contexts/DashboardPageContextProvider.test.tsx
+++ graylog2-web-interface/src/views/components/contexts/DashboardPageContextProvider.test.tsx
@@ -48,6 +48,7 @@ const emptyLocation = {
   search: '',
   hash: '',
   state: undefined,
+  key: '',
 };
 
 describe('DashboardPageContextProvider', () => {
@@ -74,7 +75,7 @@ describe('DashboardPageContextProvider', () => {
 
     contextValue.setDashboardPage('page-id');
 
-    expect(mockNavigate).toHaveBeenCalledWith('?page=page-id');
+    expect(mockNavigate).toHaveBeenCalledWith('?page=page-id', { replace: true });
   });
 
   it('should update url on page change', () => {
@@ -93,7 +94,7 @@ describe('DashboardPageContextProvider', () => {
 
     contextValue.setDashboardPage('page-id');
 
-    expect(mockNavigate).toHaveBeenCalledWith('?page=page-id');
+    expect(mockNavigate).toHaveBeenCalledWith('?page=page-id', { replace: true });
   });
 
   it('should unset a page from url', () => {
@@ -112,7 +113,7 @@ describe('DashboardPageContextProvider', () => {
 
     contextValue.unsetDashboardPage();
 
-    expect(mockNavigate).toHaveBeenCalledWith('');
+    expect(mockNavigate).toHaveBeenCalledWith('', { replace: true });
   });
 
   it('should not set to an unknown page', () => {
@@ -131,6 +132,6 @@ describe('DashboardPageContextProvider', () => {
 
     contextValue.setDashboardPage('new');
 
-    expect(mockNavigate).toHaveBeenCalledWith('');
+    expect(mockNavigate).toHaveBeenCalledWith('', { replace: true });
   });
 });
