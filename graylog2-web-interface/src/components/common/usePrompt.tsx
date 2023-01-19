573fe9a35bc410d3cc4bb67bfc038b9d49d62e44
Dennis Oelkers <dennis@graylog.com>
2023-01-19 08:48:46 +0100

    Simplifying `usePrompt`.

--- graylog2-web-interface/src/components/common/usePrompt.tsx
+++ graylog2-web-interface/src/components/common/usePrompt.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useEffect } from 'react';
-import { unstable_useBlocker as useBlocker } from 'react-router-dom';
+import { unstable_usePrompt } from 'react-router-dom';
 
 /**
  * Prompts the user with an Alert before they leave the current screen.
@@ -7,25 +6,6 @@ import { unstable_useBlocker as useBlocker } from 'react-router-dom';
  * @param  message
  * @param  when
  */
-const usePrompt = (message: string, when: boolean = true) => {
-  // eslint-disable-next-line no-alert
-  const blocker = useBlocker(when);
-
-  const prompt = useCallback(() => window.confirm(message), [message]);
-
-  useEffect(() => {
-    if (blocker.state === 'blocked' && !when) {
-      blocker.reset();
-    }
-
-    if (blocker.state === 'blocked' && when) {
-      if (prompt()) {
-        blocker.proceed();
-      } else {
-        blocker.reset();
-      }
-    }
-  }, [blocker, prompt, when]);
-};
+const usePrompt = (message: string, when: boolean = true) => unstable_usePrompt({ when, message });
 
 export default usePrompt;
